5dbb6d5daf10655c16ef629154e167b17e38c027
Omi6t <61189591+Omi6t@users.noreply.github.com>
2020-02-20 16:22:53 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -119,7 +119,8 @@ def login():
 	except (KeyError,IOError):
 		os.system('clear')
 		print logo
-		print('		\033[1;96m【\x1b[1;93mLOGIN WITH FACEBOOK\x1b[1;96m】' )
+		print('	    \033[1;96m【\x1b[1;93mLOGIN WITH FACEBOOK\x1b[1;96m】' )
+		print('  \033[1;91mWarning: Do Not Use Your Personal Account' )
 		id = raw_input('\033[1;96m[+] \x1b[1;93mID/Email \x1b[1;91m: \x1b[1;92m')
 		pwd = raw_input('\033[1;96m[+] \x1b[1;93mPassword \x1b[1;91m: \x1b[1;92m')
 		tik()
