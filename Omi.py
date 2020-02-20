736b8b833fd56ab11742f80dad6d386c816c8799
Omi6t <61189591+Omi6t@users.noreply.github.com>
2020-02-20 17:36:28 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -17,7 +17,7 @@ br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U
 
 
 def keluar():
-	print "\033[1;96m[!] \x1b[1;91mExit"
+	print "\033[1;91m✘\x1b[1;91mExit"
 	os.sys.exit()
 
 
