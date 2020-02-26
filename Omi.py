52f70499277560e9a8607efee7c64a8af57bd3f2
Omi6t <61189591+Omi6t@users.noreply.github.com>
2020-02-26 16:50:33 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -98,9 +98,9 @@ CorrectPassword = "Omi"
 
 loop = 'true'
 while (loop == 'true'):
-    username = raw_input("\033[1;96m🔐 \x1b[1;93mTool Username \x1b[1;93m»»\x1b[1;96m")
+    username = raw_input("\033[1;96m🔐 \x1b[1;93mTool Username\x1b[1;93m»» \x1b[1;96m")
     if (username == CorrectUsername):
-    	password = raw_input("\033[1;96m🔐 \x1b[1;93mTool Password \x1b[1;93m»»\x1b[1;96m")
+    	password = raw_input("\033[1;96m🔐 \x1b[1;93mTool Password\x1b[1;93m»» \x1b[1;96m")
         if (password == CorrectPassword):
             print "Logged in successfully as " + username
             loop = 'false'
@@ -120,7 +120,7 @@ def login():
 		os.system('clear')
 		print logo
 		print('	   \033[1;96m【\x1b[1;96mLOGIN WITH FACEBOOK\x1b[1;96m】' )
-		jalan('  \033[1;91mWarning:\033[1;93mDo Not Use Your Personal Account' )
+		jalan(' \033[1;91mWarning: \033[1;93mDo Not Use Your Personal Account' )
 		jalan('       \033[1;93mUse a Fresh Account To Login' )
 		id = raw_input('\033[1;96m[+] \x1b[1;93mID/Email\x1b[1;93m: \x1b[1;96m')
 		pwd = raw_input('\033[1;96m[+] \x1b[1;93mPassword\x1b[1;93m: \x1b[1;96m')
