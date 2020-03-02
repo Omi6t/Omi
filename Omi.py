9dac5de8b4126bf5756419e9b6e8805d9215a90d
Omi6t <61189591+Omi6t@users.noreply.github.com>
2020-03-02 16:23:30 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -88,7 +88,7 @@ print  """
                  {=  ^  =}
                   >  -  <
  _____________.""`-------`"".______________
-| \033[1;92m     Process Has Been Completed     |
+| \033[1;92m       Process Has Been Completed       |
     \033[1;92mCP File Has Been Saved\033[1;92m:\033[1;97mout/checkpoint.txt
     
         \033[1;93m       (           )    \033[1;91m*         
