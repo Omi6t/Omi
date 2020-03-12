338dce31ba118e5c414bc2bc5ce70690eea87a73
Omi Chaudhary <61189591+Omi6t@users.noreply.github.com>
2020-03-12 17:32:12 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -95,10 +95,10 @@ print  """
 \033[1;91mInstagram\033[1;91m: \033[1;93mhttps://www.Instagram.com/Omi6t
 \033[1;91mFacebook\033[1;91m: \033[1;93mhttps://www.facebook.com/Omi6t
 \033[1;91mWhatsapp\033[1;91m: \033[1;93m+923117675174
-\033[1;91m«-----------------------\033[1;93m✧✧\033[1;91m-----------------------»"""
+\033[1;91m«-----------------------\033[1;93m✧✧\033[1;91m-----------------------»\033[1;97m"""
 jalan("\033[7;91m Disclaimer:\033[0m\033[1;97mDeveloper Assume No Liability and Not")
 jalan("\033[1;97m            Responsible for any Misuse or Damage.")
-print "«-----------------------\033[1;93m✧✧\033[1;91m-----------------------»"
+print "\033[1;91m«-----------------------\033[1;93m✧✧\033[1;91m-----------------------»\033[1;97m"
 print "                 \033[1;41mREAD CAREFULLY\033[0m"
 print "\033[1;97mIt is not in Commands Control to get a New or Old"
 print "\033[1;97mAccount,It Totally Depends on Friends List And the"
